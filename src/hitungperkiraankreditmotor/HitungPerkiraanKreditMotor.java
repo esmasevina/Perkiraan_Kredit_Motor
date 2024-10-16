d0e1fc539cb1cc4a3d1852ec03df3b2393fad310
esmasevina <esmanursevina78@gmail.com>
2024-10-16 16:25:36 +0700

    Program sederhana belajar java dengan program perkiraan kredit motor

---
+++ src/hitungperkiraankreditmotor/HitungPerkiraanKreditMotor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hitungperkiraankreditmotor;
+
+/**
+ *
+ * @author Ardy Sendleep
+ */
+public class HitungPerkiraanKreditMotor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame_perkiraan_kredit_motor Laund = new Frame_perkiraan_kredit_motor();
+        Laund.setVisible(true);
+    }
+}
